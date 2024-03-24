779e5ce8047209a6f084eeb8ea8dce1064fb34d0
novakvova <novakvova@gmail.com>
2024-03-24 09:40:57 +0200

    Change Database in Sqlite

--- Rozetka/DAL/Data/EFAppContext.cs
+++ Rozetka/DAL/Data/EFAppContext.cs
@@ -23,9 +23,9 @@ namespace DAL.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseNpgsql("Server=localhost;Port=5432;Database=revo;User Id=postgres;Password=123456;");
-                //.UseSqlite("DataSource=revo.db;");
-               // .UseSqlServer("Server=.;Database=revo;Trusted_Connection=True;TrustServerCertificate=true;");
+                //.UseNpgsql("Server=localhost;Port=5432;Database=revo;User Id=postgres;Password=123456;");
+                .UseSqlite("DataSource=revo.db;");
+            // .UseSqlServer("Server=.;Database=revo;Trusted_Connection=True;TrustServerCertificate=true;");
             //.UseSqlServer("Data Source=20.65.144.204;User ID=kaban;Password=9[nV`e7VN`0%;Initial Catalog=revo;MultipleActiveResultSets=true;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -45,16 +45,18 @@ namespace DAL.Data
                 ur.HasKey(b => new { b.UserId, b.RoleId });
             });
 
-            modelBuilder.Entity<CategoryEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<ProductEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<ProductImageEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<UserEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<OrderStatusEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<OrderItemEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<OrderEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<SaleEntity>().HasQueryFilter(p => !p.IsDelete);
-            modelBuilder.Entity<Sales_ProductEntity>().HasQueryFilter(p => DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc) < p.Sale.ExpireTime);
-            modelBuilder.Entity<RoleEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<CategoryEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<ProductEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<BasketEntity>().HasQueryFilter(p => !p.Product.IsDelete);
+            //modelBuilder.Entity<ProductImageEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<UserEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<OrderStatusEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<OrderItemEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<OrderEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<SaleEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<Sales_ProductEntity>().HasQueryFilter(p => DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc) < p.Sale.ExpireTime);
+            //modelBuilder.Entity<RoleEntity>().HasQueryFilter(p => !p.IsDelete);
+            //modelBuilder.Entity<UserRoleEntity>().HasQueryFilter(p => !p.Role.IsDelete);
         }
     }
 }
